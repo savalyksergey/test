0c94948413508069df2a39b54e7dc09545da4c4a
savalyksergey <voron9@gmail.com>
2016-11-18 17:04:47 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+ <title>www.lab.jelastic.com</title>
+  </head>
+   <body>
+    <h1>Hello world!</h1>
+    <?php
+    echo "<p>The time is " . date("h:i:sa") ."<\p>";
+    ?>
+    trytrytry
+     </body>
